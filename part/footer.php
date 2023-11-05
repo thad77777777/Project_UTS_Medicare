c61cb38f102e6272f92b17e6bbd8e52ff80487da
thad77777777 <thad.mf77@gmail.com>
2023-11-05 11:57:02 +0700

    first commit

---
+++ part/footer.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      &copy; <div class="bullet"></div> <a href='' title='' target='_blank'></a>
+      
+      
+      
+    </div>
+    <div class="footer-right">
+
+    </div>
+  </footer>
